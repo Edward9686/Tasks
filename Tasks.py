137c3610e5012be80b732ef7b5ae05661574718c
Edward9686 <ink.sans00@mail.ru>
2024-07-23 01:30:09 +0700

    Add files via upload

---
+++ Tasks.py
@@ -0,0 +1,8 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print(1234 % 1000 //10 + 5678 % 1000 //10)
+# 4th program
+print(13 == 13 or 42 == 42)
